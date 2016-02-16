845e04dbbaf29c6da63d3c6ac076852c3ea70110
skymoneyc <CChain0615@gmail.com>
2016-02-16 23:23:23 +0800

    update login post

--- node_crawler/main.js
+++ node_crawler/main.js
@@ -5,6 +5,7 @@
 var request = require("request");
 var cheerio = require("cheerio");
 var iconv = require("iconv-lite");
+var querystring = require("querystring");
 
 var home_seed = "http://www.zhihu.com/explore";
 
@@ -14,7 +15,26 @@ var main_page = function(url, callback){
 			console.log("抓取首页失败");
 		}
 
+		if(response.statusCode != 200){
+			console.log("响应状态错误");
+		}
+
 		callback(cheerio.load(iconv.decode(body, "utf-8"), {decodeEntities: false}));
+	});
+}
+
+var login_func = function(url, form_data, callback){
+	request.post({uri: url, body: querystring.stringify(form_data)}, function(error, response, body){
+		if(error){
+			console.log("提交失败");
+		}
+
+		if(response.statusCode != 200){
+			console.log("status code invalid");
+		}
+
+		console.log(response.cookies);
+		callback(response);
 	});
 }
 
@@ -22,6 +42,11 @@ var main_page = function(url, callback){
 var main_func = function(callback){
 	//爬取首页
 	main_page(home_seed, function(dom){
-		
+
 	})
 }
+
+login_func('http://www.zhihu.com/login', {'email': 'cchain0615@gmail.com', 'password': 'kgbfbi'}, 
+	function(data){
+		console.log(data);
+})
